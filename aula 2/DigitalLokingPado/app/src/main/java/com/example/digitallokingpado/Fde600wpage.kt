4c51df1d8591587f10ab184f7a7822f556c527df
JOAO-VITOR-SOARES <jogalijando@gmail.com>
2022-08-26 17:37:38 -0300

    :books aula02

---
+++ aula 2/DigitalLokingPado/app/src/main/java/com/example/digitallokingpado/Fde600wpage.kt
@@ -0,0 +1,14 @@
+package com.example.digitallokingpado
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Fde600wpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fde600wpage)
+
+    }
+}
