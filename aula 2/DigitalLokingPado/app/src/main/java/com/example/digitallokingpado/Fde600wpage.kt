57fb0ee9d937b3af5728c6cdd5dc6660058d999e
JOAO-VITOR-SOARES <jogalijando@gmail.com>
2022-08-31 17:38:31 -0300

    :iphone:mobile development exercise

--- aula 2/DigitalLokingPado/app/src/main/java/com/example/digitallokingpado/Fde600wpage.kt
+++ aula 2/DigitalLokingPado/app/src/main/java/com/example/digitallokingpado/Fde600wpage.kt
@@ -9,6 +9,5 @@ class Fde600wpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fde600wpage)
-
     }
 }
--- aula 2/DigitalLokingPado/app/src/main/java/com/example/digitallokingpado/MainActivity.kt
+++ aula 2/DigitalLokingPado/app/src/main/java/com/example/digitallokingpado/MainActivity.kt
@@ -4,16 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainTollBar = findViewById<MaterialToolbar>(R.id.mainToolbar)
 
-        val btn = findViewById<Button>(R.id.button)
 
-        btn.setOnClickListener {
-            startActivity(Intent(this,Fde600wpage::class.java))
+        mainTollBar.setNavigationOnClickListener(){
+            Toast.makeText(this,
+                "Hamburger pressionado!!!",
+                Toast.LENGTH_SHORT)
+                    .show()
+
+        }
+
+        mainTollBar.setOnMenuItemClickListener{ menuItem ->
+            when (menuItem.itemId)
+            {
+                R.id.menu_item_notifications -> {
+                    Toast.makeText(
+                        this,
+                        "Ir para pagina de notificações",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                R.id.menu_item_cart -> {
+                    Toast.makeText(
+                        this,
+                        "Ir para pagina do carrinho",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+
+                }
+                else -> false
+            }
         }
     }
 }
